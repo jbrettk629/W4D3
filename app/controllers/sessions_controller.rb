83349f25d307751ee3a838dc5cf334c74943db2c
jbrettk629 <jbrettk629@gmail.com>
2018-09-12 14:41:24 -0700

    Finish layout form for 99Cats

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  before_action :require_login, only: [:destroy]
+  before_action :require_logout, only: [:new, :create]
+  
+  def new
+    @user = User.new
+    render :new
+  end
+  
+  def create 
+    @user = user.find_by_credentials(
+                params[:user][:username],
+                params[:user][:password])
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ['Invalid Login Credentials']
+      render :new
+    end
+  end  
+  
+  def destroy 
+    logout! 
+    redirect_to new_user_url
+  end 
+  
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord 
+  attr_reader :password
+  validates :password_digest, :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_null: true }
+  validates :session_token, presence: true
+  
+  after_initialize :ensure_session_token
+  
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+  
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+  
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+  
+  def self.find_by_credentials(user_name,pw)
+    user = User.find_by(username: user_name)
+    return nil if user.nil?
+    is_password?(pw) ? user : nil
+  end
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end 
